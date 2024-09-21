faba41ff821241e1649f2dad5b7063aaa550ae58
Hubert Bourget <hubbourget@hotmail.com>
2024-09-20 20:20:45 -0600

    deploying fetchRecommendations on staging

--- frontend/src/pageComponents/Library.js
+++ frontend/src/pageComponents/Library.js
@@ -10,53 +10,11 @@ export default function Library() {
   // const isAuthenticated = true;
   // const user ={ name: "debug9@debug.com" }
 
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    const fetchRecommendations = async () => {
-        try {
-            if (user) {
-                console.log('user :', user);
-                const recoResponse = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/getItemToUserRecommendations/${user}`);
-                // console.log("recoResponse: ", recoResponse )
-                const objectIds = recoResponse.data.recomms.map(recomm => recomm.id);
-                // console.log("VideoIds: ", videoIds);
-                const list = []
-                const videosData = await Promise.allSettled(objectIds.map(async (id) => {
-                    const videoResp = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/getVideoMetaDataFromObjectId/${id}`);
-                    const videoData = videoResp.data;
-                    // console.log("videoData: ", videoData);
-                    const userResp = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/getUserProfile/${videoData.owner}`);
-                    const userData = userResp.data;
-                    if(videoData){
-                        list.push({
-                            ...videoData,
-                            accountName: userData.accountName // Assume this field exists in your user response
-                        })
-                    }
-
-                    return {
-                        ...videoData,
-                        accountName: userData.accountName // Assume this field exists in your user response
-                    };
-                }));
-                
-                console.log("🚀 ~ fetchRecommendations ~ list:", list)  
-                setVideos(list);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    fetchRecommendations();
-    }, [user]);
-
   const context = useOutletContext();
   const { isSearched, result } = context;
   // useEffect(() => {
   //   console.log(result)
   // }, [isSearched, result])
-  // const user = { name: "debug9@debug.com" };
   const [filter, setFilter] = useState("all");
   const [contents, setContents] = useState([]);
   const [allContent, setAllContent] = useState([]);
@@ -147,7 +105,7 @@ export default function Library() {
     } else if (filter === "all") {
       fetchData(filter, setAllContent);
       fetchRecommendations();
-      fetchEvents();
+      // fetchEvents();
     }
   }, [filter]);
   return (
